28c0a22f8d5ff09a73c13d9eacf2230342087853
Innokentii Kozlov <innokentiikozlov@gmail.com>
2026-07-25 16:49:49 +0000

    remove sqlite db. (#5)
    Reviewed-on: https://forgejo.innokentii-kozlov.com/innokentii-kozlov/clyft/pulls/5

--- src/clyft/service/init/__init__.py
+++ src/clyft/service/init/__init__.py
@@ -1,18 +1,14 @@
 from clyft.service.init.main import (
-    CLYFT_DATABASE_NAME,
     ROOT_USER_CLYFT_PATH,
     USER_CLYFT_PATH,
-    create_clyft_database,
     create_clyft_path,
     get_clyft_path,
     init,
 )
 
 __all__ = [
-    "CLYFT_DATABASE_NAME",
     "ROOT_USER_CLYFT_PATH",
     "USER_CLYFT_PATH",
-    "create_clyft_database",
     "create_clyft_path",
     "get_clyft_path",
     "init",
--- src/clyft/service/init/main.py
+++ src/clyft/service/init/main.py
@@ -1,10 +1,8 @@
 import getpass
-import sqlite3
 from pathlib import Path
 
 USER_CLYFT_PATH = ".local/share/clyft/"
 ROOT_USER_CLYFT_PATH = "/var/lib/clyft/"
-CLYFT_DATABASE_NAME = "database.db"
 
 
 def _get_current_user() -> str:
@@ -21,16 +19,6 @@ def create_clyft_path() -> None:
     Path(get_clyft_path()).mkdir(parents=True, exist_ok=True)
 
 
-def create_clyft_database() -> None:
-    database_path = Path(get_clyft_path()) / CLYFT_DATABASE_NAME
-    connection = sqlite3.connect(database_path)
-    try:
-        pass
-    finally:
-        connection.close()
-
-
 def init() -> str:
     create_clyft_path()
-    create_clyft_database()
     return get_clyft_path()
